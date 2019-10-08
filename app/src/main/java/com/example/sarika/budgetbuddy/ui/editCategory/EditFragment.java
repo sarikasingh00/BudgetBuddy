cb9ce87507f811d1de6f34bb606a68a7a00d2b57
Yash Shah <yashcshah30@gmail.com>
2019-10-08 20:26:24 +0530

    EditCategory(Working properly)

--- app/src/main/java/com/example/sarika/budgetbuddy/ui/editCategory/EditFragment.java
+++ app/src/main/java/com/example/sarika/budgetbuddy/ui/editCategory/EditFragment.java
@@ -1,5 +1,7 @@
 package com.example.sarika.budgetbuddy.ui.editCategory;
 
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
@@ -8,15 +10,33 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.sarika.budgetbuddy.R;
+import com.example.sarika.budgetbuddy.ui.addCategory.AddFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class EditFragment extends Fragment {
 
     private EditViewModel mViewModel;
+    private TextView updatebudget;
+    private TextView edittext;
+    private Button updatebutton;
+    private Spinner spinner;
+    String spinnertext;
 
     public static EditFragment newInstance() {
         return new EditFragment();
@@ -32,7 +52,50 @@ public class EditFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(EditViewModel.class);
+        Log.d("Doc", "mViewModel-" + mViewModel);
+        mViewModel.getCat().observe(getViewLifecycleOwner(), new Observer<String[]>() {
+            @Override
+            public void onChanged(String[] strings) {
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(Objects.requireNonNull(getActivity()), android.R.layout.simple_spinner_dropdown_item, mViewModel.cat.getValue());
+                spinner.setAdapter(adapter);
+                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        spinnertext = adapterView.getItemAtPosition(i).toString();
+                        //int expense = mViewModel.getexpense(spinnertext);
+                       // Toast.makeText(adapterView.getContext(), spinnertext, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                });
+            }
+        });
         // TODO: Use the ViewModel
     }
 
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        updatebudget = view.findViewById(R.id.budget_update);
+        edittext = view.findViewById(R.id.EditCategory);
+        updatebutton = view.findViewById(R.id.update_button);
+        spinner = view.findViewById(R.id.spinner1);
+    }
+
+    public void onStart() {
+        super.onStart();
+        mViewModel.getData();
+        updatebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int budgettoupdate = Integer.parseInt(updatebudget.getText().toString());
+                mViewModel.updateData(spinnertext, budgettoupdate);
+                updatebudget.setText("");
+                Toast.makeText(getActivity(), "Category Updated!",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 }
+
+
